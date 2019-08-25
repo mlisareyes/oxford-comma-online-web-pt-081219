2819820b904c2bad1b48c9706ea3dd18a35e2522
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-25 03:59:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.first
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    "#{array[0..-2].join(", ")}, and #{array.last}"
+  end
 end
